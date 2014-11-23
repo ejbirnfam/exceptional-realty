868503a4779851280debb3556f4184ff7af73843
ejbirnfam <ericjbirnbaum@gmail.com>
2014-11-23 12:37:25 -0500

    added form validation

--- js/main.js
+++ js/main.js
@@ -14,4 +14,58 @@ $(function() {
     }
   });
 
+  ///////// Contact Form Validation \\\\\\\\\
+
+  function validateName(fullname) {
+    if(fullname.length >2) {
+      $('#fullname').removeClass('error');
+      $('#fullname-error').hide();
+      return true;
+    }
+    else {
+      $('#fullname').addClass('error');
+      $('#fullname-error').show();
+      return false;
+    }
+  }
+
+  function validateEmail(email) {
+    var re = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+    if(re.test(email)) {
+      $('#email').removeClass('error');
+      $('#email-error').hide();
+      return true;
+    }
+    else {
+      $('#email').addClass('error');
+      $('#email-error').show();
+      return false;
+    }
+  }
+
+  function validateMessage(message){
+    if (message.length > 0) {
+      $('#message').removeClass('error');
+      $('#message-error').hide();
+    }
+    else {
+      $('#message').addClass('error');
+      $('#message-error').show();
+      return false;
+    }
+  }
+
+  $('form').submit(function(event){
+
+    var fullname = $('#fullname').val(),
+        email = $('#email').val(),
+        message = $.trim($('#message').val()); // gets rid of spaces and line returns
+
+        if (validateName(fullname) & validateEmail(email) & validateMessage(message)) {
+          return true; //submit form
+        }
+        else {
+          event.preventDefault(); //prevent form from submitting
+        }
+  });
 });
